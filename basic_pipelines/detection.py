92e3ee3ffd9b372bf47fa20cc590ca1b6756a831
Erie M. Adames <erieadames@gmail.com>
2024-11-23 13:39:57 -0500

    test

--- basic_pipelines/detection.py
+++ basic_pipelines/detection.py
@@ -60,7 +60,7 @@ def app_callback(pad, info, user_data):
         bbox = detection.get_bbox()
         confidence = detection.get_confidence()
         if label == "person":
-            string_to_print += f"Detection: {label} {confidence:.2f} {frame.shape}\n"
+            string_to_print += f"Detection: {label} {confidence:.2f}\n"
             detection_count += 1
     if user_data.use_frame:
         # Note: using imshow will not work here, as the callback function is not running in the main thread
@@ -70,7 +70,6 @@ def app_callback(pad, info, user_data):
         # Let's print the new_variable and the result of the new_function to the frame
         cv2.putText(frame, f"{user_data.new_function()} {user_data.new_variable}", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         # Convert the frame to BGR
-        print(frame.shape)
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
         user_data.set_frame(frame)
 
